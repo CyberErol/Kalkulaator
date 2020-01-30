499fb3f128e082745c99c6337197b64c07f344cd
CyberErol <58256556+CyberErol@users.noreply.github.com>
2020-01-30 14:55:49 +0200

    Add files via upload

---
+++ Eroli kood/script.js
@@ -0,0 +1,7 @@
+function pluss(){
+    document.getElementById("col2").value = document.getElementById("clahter1").value;
+}
+
+function miinus(){
+    document.getElementById("col3").value = document.getElementById("lahter2").value;
+}
