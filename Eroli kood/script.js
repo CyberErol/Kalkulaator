e185a1631e8ad28d4762f5593a5c072f4ddb861e
CyberErol <58256556+CyberErol@users.noreply.github.com>
2020-01-31 11:10:55 +0200

    Add files via upload

--- Eroli kood/script.js
+++ Eroli kood/script.js
@@ -1,7 +1,20 @@
-function pluss(){
-    document.getElementById("col2").value = document.getElementById("clahter1").value;
-}
-
-function miinus(){
-    document.getElementById("col3").value = document.getElementById("lahter2").value;
-}
+var vahe = 0
+        document.getElementById("vahe").innerHTML = vahe
+        function pluss() {
+          var btn = document.createElement("li");
+          btn.classList.add('positive')
+          btn.innerHTML = document.getElementById("myText").value
+          document.getElementById("tulud_values").appendChild(btn)
+          var tulu = +document.getElementById("myText").value
+          vahe = window.vahe + tulu
+          document.getElementById("vahe").innerHTML = vahe
+        }
+        function miinus() {
+          var btn = document.createElement("li");
+          btn.classList.add('negative')
+          btn.innerHTML = document.getElementById("myText").value
+          document.getElementById("kulud_values").appendChild(btn)
+          var tulu = document.getElementById("myText").value;
+          vahe = window.vahe - tulu
+          document.getElementById("vahe").innerHTML = vahe
+        }
